bf3a001dbf3dec1b0ed02c24dc744ab626f7e32f
Janne Häkkilä <janne.hakkila@student.oulu.fi>
2026-05-07 19:00:13 +0300

    Exception added. Ready.

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,6 +2,9 @@ import numpy as np
 
 def calculate(list):
 
+    if len(list) < 9:
+        raise ValueError("List must contain nine numbers.")
+
     array = np.array(list).reshape(3, 3)
 
     calculations = {
