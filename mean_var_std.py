92d3ae24393a50fcfb70dbc572c8d29509ceacba
Janne Häkkilä <janne.hakkila@student.oulu.fi>
2026-05-07 18:33:58 +0300

    Mean done.

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,11 @@ import numpy as np
 
 def calculate(list):
 
+    array = np.array(list).reshape(3, 3)
 
+    calculations = {
+        'mean': [np.mean(array, axis = 0).tolist(), np.mean(array, axis = 1).tolist(), np.mean(array).tolist()]
+    }
 
 
     return calculations
